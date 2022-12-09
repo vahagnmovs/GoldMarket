fa22cbaaafb90c97743b6f36b6937727de28ea16
sipanstepanyan04 <sipanstepanyan@Sipans-MacBook-Pro.local>
2022-12-09 16:06:15 +0400

    styles

--- goldmarket/src/components/Footer/footerEnd/index.js
+++ goldmarket/src/components/Footer/footerEnd/index.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import copyrightIcon from "../../../Icons/copyright.svg"
-import "./footre_end.scss"
+import React from "react";
+import copyrightIcon from "../../../Icons/copyright.svg";
+import "./footre_end.scss";
 
 const FooterEnd = () => {
-    return (
-        <div>
-
-            <div className={"footer_text"}>
-                <div className={"icon"}>
-                    <img className={"copyrightIcon"} src={copyrightIcon} alt=""/>
-                    <span>2022 Golden Center </span>
-                </div>
-
-                    <p>Terms od Use</p>
-                    <p>TPrivacy Policy</p>
-                    <p>Sitemap</p>
-                </div>
+  return (
+    <div>
+      <div className={"footer_text"}>
+        <div className={"icon"}>
+          <img className={"copyrightIcon"} src={copyrightIcon} alt="" />
+          <span>2022 Golden Center </span>
         </div>
-    );
+
+        <p>Terms od Use</p>
+        <p>TPrivacy Policy</p>
+        <p>Sitemap</p>
+      </div>
+    </div>
+  );
 };
 
 export default FooterEnd;
