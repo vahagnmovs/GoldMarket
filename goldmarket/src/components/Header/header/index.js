faed14a0c47af79bb5d3b44b9bd156de82cfd8c2
DarkMan9899 <davitkirakosyan99@gmail.com>
2022-12-10 19:07:26 +0400

    Header style

--- goldmarket/src/components/Header/header/index.js
+++ goldmarket/src/components/Header/header/index.js
@@ -5,7 +5,10 @@ import close from "../../../style/Icons/close.png"
 import heart from "../../../style/Icons/heart.png"
 import user from "../../../style/Icons/user.png"
 import basket from "../../../style/Icons/basket.png"
-import "./Header.scss"
+import "../../../style/elements/_header.scss"
+import "../../../style/base/_globals.scss"
+import "../../../style/base/_reset.scss"
+import "../../../style/utilities/_variables.scss"
 
 const flags = [
     {
@@ -43,7 +46,7 @@ const currency = [
     }
 ];
 
-const  Header = () =>{
+const Header = () => {
 
     const [flagState, setFlagState] = useState(flags);
     const [flagChoose, setFlagChoose] = useState(true);
@@ -65,23 +68,23 @@ const  Header = () =>{
 
     return (
         // containrt
-        <div className={"container"}>
-            {/*container_top*/}
-            <div className={"container_top"}>
+        <div className={"container flex"}>
+            {/*header*/}
+            <header className={"header flex justify-between align-center"}>
                 {/*top_left_content*/}
-                <div className={"top_left_content"}>
+                <div className={"top_left_content flex justify-between align-center"}>
                     {/*flag_content*/}
-                    <div className={"flag_content"}>
+                    <div className={"flag_content flex"}>
 
                         {
                             flagChoose
                                 ?
                                 // flags
-                                <ul className={"flags"} onClick={() => setFlagChoose(false)}>
+                                <ul className={"flags flex align-center"} onClick={() => setFlagChoose(false)}>
                                     {/*flag_img*/}
-                                   <li> <img className={"flag_img"} src={flagState[id].url} alt="flag_img"/></li>
+                                    <li><img className={"flag_icon"} src={flagState[id].url} alt="flag_img"/></li>
                                     {/*language*/}
-                                   <li><span className={"language"}>{flagState[id].value}</span></li>
+                                    <li><span className={"language"}>{flagState[id].value}</span></li>
                                     {/*drop_down*/}
                                     <li><img className={"drop_down"} src={open} alt="drop_down"/></li>
 
@@ -91,10 +94,12 @@ const  Header = () =>{
                                     {
                                         flagState.map(flag => {
                                             return (
-                                                <ul className={"flags"} onClick={() => handleChangeLanguage(flag.id)}>
-                                                 <li> <img className={"flag_img"} src={flag.url} alt="flag_img"/></li>
-                                                 <li><span className={"language"}>{flag.value}</span>  </li>
-                                                 <li>  <img className={"drop_up"}src={close} alt="drop_up"/>  </li>
+                                                <ul className={"flags flex  align-center"}
+                                                    onClick={() => handleChangeLanguage(flag.id)}>
+                                                    <li><img className={"flag_icon"} src={flag.url} alt="flag_img"/>
+                                                    </li>
+                                                    <li><span className={"language"}>{flag.value}</span></li>
+                                                    <li><img className={"drop_up"} src={close} alt="drop_up"/></li>
                                                 </ul>
                                             )
                                         })
@@ -102,16 +107,16 @@ const  Header = () =>{
                         }
                     </div>
                     {/*valuta_content*/}
-                    <div className={"valuta_content"}>
+                    <div className={"valuta_content flex align-center"}>
                         {
                             isChoseCurrency
                                 ?
-                                // flags
-                                <ul className={"flags"} onClick={() => setIsChoseCurrency(false)}>
+                                // value
+                                <ul className={"valuta flex align-center"} onClick={() => setIsChoseCurrency(false)}>
                                     {/*language*/}
-                                <li>  <span className={"language"}>{currencyState[currencyId].currency} </span></li>
+                                    <li><span className={"language"}>{currencyState[currencyId].currency} </span></li>
                                     {/*drop_down*/}
-                                <li><img className={"drop_down"} src={open} alt="drop_down"/> </li>
+                                    <li><img className={"drop_down "} src={open} alt="drop_down"/></li>
                                 </ul>
                                 :
                                 <div>
@@ -119,9 +124,9 @@ const  Header = () =>{
                                         currencyState.map(currency => {
                                             return (
                                                 // flags
-                                                <ul key={currency.id} className={"flags"}
-                                                     onClick={() => handleChangeCurrency(currency.id)}>
-                                                 <li><span>{currency.currency}</span></li>
+                                                <ul key={currency.id} className={"valuta flex align-center"}
+                                                    onClick={() => handleChangeCurrency(currency.id)}>
+                                                    <li><span>{currency.currency}</span></li>
                                                     {/*drop_up*/}
                                                     <li><img className={"drop_up"} src={close} alt=""/></li>
                                                 </ul>
@@ -132,39 +137,35 @@ const  Header = () =>{
                         }
                     </div>
                 </div>
-            </div>
-
-            {/*site_logo*/}
-           <div className={"site_logo"}>
-               <img src={logo} alt={"site_logo"} className={"logo"}/>
-           </div>
-
-
-            {/*top_right_content*/}
-            <div className={"top_right_content"}>
-                {/*wishlist*/}
-                <div className={"wishlist"}>
-                    {/*text_and_heart*/}
-                    <div className={"text_and_heart"}>
-                        <span>WISHLIST</span>
-                        <img className={"icon_favorite"} src={heart} alt="icon_favorite"/>
-                        <span>(0)</span>
-                    </div>
-
-                </div>
-                {/*user_login*/}
-                <div className={"user_login"}>
-                    <spam>LOGIN</spam>
-                    <img className={"user_icon"} src={user} alt="user_icon"/>
+                {/*site_logo*/}
+                <div className={"site_logo "}>
+                    <h2>
+                        <img src={logo} alt={"site_logo"} className={"logo"}/>
+                    </h2>
                 </div>
 
-                {/*card*/}
-                <div className={"card"}>
-                    <span>CART</span>
-                    <img className={"icon_card"} src={basket} alt="icon_card"/>
-                    <span>(0)</span>
+
+                    {/*wishlist*/}
+                <div className={"top_right_content flex align-center"}>
+                    <ul  className={"flex align-center"}>
+                        <li className={"wishlist_content flex align-center"}>
+                            <span>WISHLIST</span>
+                            <img className={"icon_favorite"} src={heart} alt="icon_favorite"/>
+                            <span>(0)</span>
+                        </li>
+                        <li className={"login_content flex align-center"}>
+                            <spam>LOGIN</spam>
+                            <img className={"user_icon"} src={user} alt="user_icon"/>
+                        </li>
+                        <li className={"cart_content flex align-center"}>
+                            <span>CART</span>
+                            <img className={"icon_card"} src={basket} alt="icon_card"/>
+                            <span>(0)</span>
+                        </li>
+                    </ul>
                 </div>
-            </div>
+
+            </header>
         </div>
     );
 };
