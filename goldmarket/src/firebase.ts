37674a53147d7a7e969338c22309f1cc3bfcfdc3
Vahagn Movsesyan <vahag999988@gmail.com>
2022-12-14 01:02:43 +0400

    added functionality to add collection to firebase

--- goldmarket/src/firebase.ts
+++ goldmarket/src/firebase.ts
@@ -1,13 +1,44 @@
-import { initializeApp } from "firebase/app";
+import {initializeApp} from "firebase/app";
+import {getFirestore, collection, getDocs, setDoc, addDoc} from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCRi3RU90XwiK6YBTK511zbXE9xpsV2sho",
-    authDomain: "goldmarket2022-a7311.firebaseapp.com",
-    projectId: "goldmarket2022-a7311",
-    storageBucket: "goldmarket2022-a7311.appspot.com",
-    messagingSenderId: "3358758741",
-    appId: "1:3358758741:web:cd77f4e0993b82fc3dc1dc"
-};
+    apiKey: "AIzaSyA-xFalv4pTSNOzLF94b_UzAfO0FtRshZ4",
+    authDomain: "goldmarket2022-2.firebaseapp.com",
+    projectId: "goldmarket2022-2",
+    storageBucket: "goldmarket2022-2.appspot.com",
+    messagingSenderId: "835428428435",
+    appId: "1:835428428435:web:edb7241c2da6257d6fbb66",
+}
 
+// type ProductsType = {
+//     id: string,
+//     name: string,
+//     Email: string
+// }
+
+// init firebase app
 const app = initializeApp(firebaseConfig);
-
+// // init services
+// const db = getFirestore(app)
+// //  collection ref
+// const colRef = collection(db, "products")
+// // get collection data
+// getDocs(colRef)
+//     .then((snapshot) => {
+//         let products: ProductsType[] = [];
+//         snapshot.docs.forEach((doc) => {
+//             products.push({
+//                 ...doc.data(),
+//                 id: doc.id,
+//                 name: doc.data().name,
+//                 Email: doc.data().Email,
+//             })
+//         })
+//         console.log(products)
+//     })
+//
+// // push data to firebase
+// const docRef = addDoc(collection(db, "products"), {
+//     name: "Sipan",
+//     email: "email@Sipan"
+// })
