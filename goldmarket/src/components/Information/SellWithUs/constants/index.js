fb16f5a7afbc21736604d829ea4bbc600f636636
vahagnmovs <109467395+vahagnmovs@users.noreply.github.com>
2022-12-17 20:33:48 +0400

    Delete goldmarket/src/components/Information directory

--- goldmarket/src/components/Information/SellWithUs/constants/index.js
+++
@@ -1,3 +0,0 @@
-export const textFromGirl = 'lorem ipsumlorem  ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum '
-
-export const srcGirl = 'https://picupmedia.com/wp-content/uploads/2018/03/Mannequins3-1.jpg'
