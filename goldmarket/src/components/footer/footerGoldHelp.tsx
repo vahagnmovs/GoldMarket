c7a55dc0eb265383e53fbcdb732b041d8d838668
DavIdHabeTyan <davidhabetyanm@gmail.com>
2022-12-18 21:13:37 +0400

    fixed code

---
+++ goldmarket/src/components/footer/footerGoldHelp.tsx
@@ -0,0 +1,19 @@
+import {TFooterData} from "../../data/footerData";
+import {TGoldCenter} from "../../data/footerData";
+import {Link} from "react-router-dom";
+
+type TFooterLinks = {
+    title: string
+    data:  TGoldCenter[],
+}
+
+const FooterGoldHelp = ({title, data }:TFooterLinks ) =>{
+    return (
+      <>
+         <li>{title}</li>
+          {data.map(link => <li key={Math.random()}> <Link to={link.path}>{link.name}</Link> </li>)}
+      </>
+    );
+};
+
+export default FooterGoldHelp;
