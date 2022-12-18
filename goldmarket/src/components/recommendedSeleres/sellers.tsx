bf5d98eb74aefd724097ecfa34a85186370ae4c6
Sasun Steanyan <sasunstepanyan777@gmail.com>
2022-12-19 01:03:56 +0400

    recommended sellers styles

--- goldmarket/src/components/recommendedSeleres/sellers.tsx
+++ goldmarket/src/components/recommendedSeleres/sellers.tsx
@@ -1,27 +1,32 @@
 import React from 'react';
 import star from "src/style/Icons/star-regular.svg";
-import {DataSeller} from "src/data/recommendedSellers";
+import { DataSeller } from "src/data/recommendedSellers";
 import firebase from "firebase/compat";
 
 const Sellers = (product: DataSeller) => {
-    const {id, name, title, images} = product
+    const { id, name, title, images } = product
 
     return (
+        <div className={"recommender_sellers_block"}>
+            <div className="list_recommender_jewelry_img">
 
-        <div className={"block_recommender_child"}>
-            <div className={"block_recommender_child"}>
-                {images.map((image) => <img
-                    className={"block_recommender_child_jewelry"}
-                    src={image}
-                    alt=""
-                />
-                )}
-                <div>
-                    <h4>{name}</h4>
-                    <span>{title}</span>
+                <div className={"flex flex-wrap gap_10"}>
+                    {images.map((image) => <img
+                        className={"recommender_jewelry"}
+                        src={image}
+                        alt=""
+                    />
+                    )}
                 </div>
-
             </div>
+
+            <div className='jewelry_brands flex'>
+                {/* brand img */}
+                <img src="" alt=" brand img" />
+                <h4>{name}</h4>
+                <span>{title}</span>
+            </div>
+
         </div>
     );
 };
