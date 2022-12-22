bccf3071cf5acc3c7b46e2ab2260d207301ff7c8
Arsen <sahakyan.9788@gmail.com>
2022-12-22 12:23:52 +0400

    add list in wisht list and cart

--- goldmarket/src/components/header/header/cart/index.tsx
+++ goldmarket/src/components/header/header/cart/index.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import basket from 'src/style/Icons/basket.png';
+import {useAppSelector} from '../../../../hooks/redux-hooks';
+import {ProductsTypes} from '../../../../data/products';
 
 const Cart = () => {
+	const cart = useAppSelector(state => state.users.cart);
+	const [isShowCarts, setIsShowCarts] = useState(true);
+
 	return (
-		<li className={'cart_content flex align-center'}>
-			<span>CART</span>
-			<img className={'icon_card'} src={basket} alt="icon_card"/>
-			<span>(0)</span>
-		</li>
+		<>
+			{
+				isShowCarts
+					?
+					<li className={'cart_content flex align-center'} onClick={() => setIsShowCarts(false)}>
+						<span>CART</span>
+						<img className={'icon_card'} src={basket} alt='icon_card'/>
+						<span>({cart.length})</span>
+					</li>
+					:
+					cart.map((product: ProductsTypes)=> <div key={product.productID}>{product.name}</div>)
+			}
+		</>
 	);
 };
 
