c909e3c116fabb6b68e8bb9d0e81360d0ac0c84d
ArturHovakimyan <welcomearturl@gmail.com>
2022-12-25 01:23:47 +0400

    add Edit Address

--- goldmarket/src/components/centers/buyerAdmin/deliveryAddressList/index.tsx
+++ goldmarket/src/components/centers/buyerAdmin/deliveryAddressList/index.tsx
@@ -5,52 +5,77 @@ import ModalInput from '../modall';
 
 // FIXME: hanel datan erb irakan datan unenanq
 
-const data = [{
-	id: `${Math.random()}`,
+const data = {
+	id: '1',
 	name: 'VAHAGN',
 	surname: 'MOVSESYAN',
-	streetAddress: '950 Ridge RD C25',
-	streetAddressOptional: "kejirwnvi3m4644641evr6s4v6",
 	city: 'P4332 Claymont',
 	region: 'DE 19703-3538',
 	country: 'United States',
-	phoneNumber: '(302)xxxxx67',
-	zipCode: "eijf2ijf"
-}];
+	address: [{
+		addressId: `${Math.random()}`,
+		streetAddress: '950 Ridge RD C25',
+		streetAddressOptional: "kejirwnvi3m4644641evr6s4v6",
+		phoneNumber: '(302)xxxxx67',
+		zipCode: "eijf2ijf",
+	}]
+};
 const initialState = {
+	addressId: '',
 	streetAddress: '',
 	streetAddressOptional: '',
 	phoneNumber: '',
 	zipCode: ''
 }
+
 const DeliveryAddressList = () => {
 	const [modal, setModall] = useState(false)
 	const [state, setState] = useState(data)
 	const [address, setAddress] = useState(initialState);
 	const handleNewAddress = () => {
-		setModall(!modal)
+		setModall(!modal);
 	}
+
 	const handleDelete = (id: string) => {
-		const newStart = state.filter(item => item.id !== id)
-		setState(newStart)
-		
+		const newStart = state.address.filter(item => item.addressId !== id)
+		setState({ ...state, address: newStart });
+	}
+
+
+	const handleEdit = (addressId: string) => {
+		const newStart = state.address.find(item => item.addressId === addressId)
+		handleNewAddress();
+
+		if (newStart?.addressId) {
+			setAddress({
+				addressId: newStart.addressId,
+				streetAddress: newStart.streetAddress,
+				streetAddressOptional: newStart.streetAddressOptional,
+				phoneNumber: newStart.phoneNumber,
+				zipCode: newStart.zipCode
+			});
+		}
+
 	}
 	const handleOnCange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		const name = event.target.name
 		const value = event.target.value
 		setAddress({ ...address, [name]: value })
 	}
-	const handlePush = () => {
-		state.push({
-			...data[0],
-			id: `${Math.random()}`,
-			streetAddress: address.streetAddress,
-			streetAddressOptional: address.streetAddressOptional,
-			phoneNumber: address.phoneNumber,
-			zipCode: address.zipCode
-		})
+	const handleSave = () => {
+		if(address.streetAddress.trim()){
+			state.address.push({
+				...data.address[0],
+				addressId: `${Math.random()}`,
+				streetAddress: address.streetAddress,
+				streetAddressOptional: address.streetAddressOptional,
+				phoneNumber: address.phoneNumber,
+				zipCode: address.zipCode
+			})
+		}
 		setAddress(initialState);
 		setModall(false);
+		handleDelete(address.addressId)
 	}
 	return (
 		<div>
@@ -67,13 +92,16 @@ const DeliveryAddressList = () => {
 				<ModalInput handleOnCange={handleOnCange} title={'STREET ADDRESS OPTIONAL'} inputValue={address.streetAddressOptional} name={'streetAddressOptional'} />
 				<ModalInput handleOnCange={handleOnCange} title={'ZIP CODE'} inputValue={address.zipCode} name={'zipCode'} />
 				<ModalInput handleOnCange={handleOnCange} title={'PHONE NUMBER'} inputValue={address.phoneNumber} name={'phoneNumber'} />
-				<button onClick={handlePush}>SAVE</button>
+				<button onClick={handleSave}>SAVE</button>
 			</div>}
 			<div>
 				<Dashboard title={'Delivery Address'} />
 				<button onClick={handleNewAddress}>+ ADD NEW ADDRESS</button>
 			</div>
-			{state.map(item => <DeliveryAddress handleDelete={handleDelete} radio={'radio'} key={item.streetAddress} {...item} />)}
+			{state.address.map(item => <DeliveryAddress city={state.city}
+				name={state.name} region={state.region} surname={state.surname}
+				country={state.country} handleEdit={handleEdit} handleDelete={handleDelete}
+				radio={'radio'} key={item.addressId} {...item} />)}
 		</div>
 	);
 };
